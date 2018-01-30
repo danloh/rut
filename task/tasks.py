1a6e3c52c19e8cda44a7f11308b7b1ae87f088b9
Dduo <lploh2016@gmail.com>
2018-01-29 17:05:01 -0800

    frontend update

--- task/tasks.py
+++ task/tasks.py
@@ -3,7 +3,7 @@ from app import mail
 from .celery import celery_app
 from flask import render_template
 from flask_mail import Message
-from run import app  #dev or prod
+from manage import app  #dev or prod
 
 @celery_app.task(bind=True, max_retries=3, default_retry_delay=10, ignore_result=True)
 def send_email(self, to, subject, template, **kwargs):
